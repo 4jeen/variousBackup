6524a6c168e3c914dbf3454494c12e44855256b8
Evgeniy Kulikovskiy <4jeen8@gmail.com>
2022-09-15 18:08:02 +0300

    Feat: Sql bases list processing was added

--- backup_postgres.sh
+++ backup_postgres.sh
@@ -17,49 +17,48 @@
 
 BACKUP_DIR_NAME="${HOME}/tmp"
 NUMBER_OF_DAYS_TO_HOLD_BACKUPS="14"
-SQL_USER="sammy"
-SQL_USER_PWD="mysecretpassword"
-SQL_DATABASE_NAME="sammy_db"
+SQL_DATABASE_NAME="template template2"
 SQL_SERVER_NAME="localhost"
 SQL_BACKUP_PROG="pg_dump"
-S3_BUCKET="BACKUP_BUCKET"
+S3_BUCKET="postgresql-dump-bucket"
 S3_SYNC_PROG="s3cmd"
-BACKUP_SQL_FILE_NAME=`date +"%Y-%m-%dT%H:%M_${SQL_DATABASE_NAME}.sql.gz"`
-DELETE_SQL_FILE_NAME=`date +"%Y-%m-%dT%H:??_${SQL_DATABASE_NAME}.sql.gz" -d "${NUMBER_OF_DAYS_TO_HOLD_BACKUPS} day ago"`
 
 
-
-TMP_FILE=/tmp/$0.tmp
+TMP_FILE=`echo $0|sed  's;[a-zA-Z0-9./]\+\/;;g'`
+TMP_FILE="/tmp/${TMP_FILE}.tmp"
 
 backup () {
 
-	if [ ! -f ${HOME}/.pgpass ]; then
+  if [ ! -f ${HOME}/.pgpass ]; then
 		echo ".pgpass file not found in your home dir"
 		return 1
   fi
+  for NM in ${SQL_DATABASE_NAME};do
+    BACKUP_SQL_FILE_NAME=`date +"%Y-%m-%dT%H:%M_${NM}.sql.gz"`
+    DELETE_SQL_FILE_NAME=`date +"%Y-%m-%dT%H:??_${NM}.sql.gz" -d "${NUMBER_OF_DAYS_TO_HOLD_BACKUPS} day ago"`
+    echo ${NM}
+    ${SQL_BACKUP_PROG} -h ${SQL_SERVER_NAME}  ${NM} | gzip  > ${BACKUP_DIR_NAME}/${BACKUP_SQL_FILE_NAME}
+    if [ $? -ne 0 ];then
+	return $?
+    fi
+    if [ -e ${HOME}/.s3cfg ]; then
+			${S3_SYNC_PROG} put ${BACKUP_DIR_NAME}/${BACKUP_SQL_FILE_NAME}  s3://${S3_BUCKET}/${BACKUP_SQL_FILE_NAME}
+    fi
 
-	${SQL_BACKUP_PROG} -h ${SQL_SERVER_NAME} -U ${SQL_USER} ${SQL_DATABASE_NAME} | gzip  > ${BACKUP_DIR_NAME}/${BACKUP_SQL_FILE_NAME}
-	if [ $? -ne 0 ];then
-		return $?
-	fi
-	if [ -e ${HOME}/.s3cfg ]; then
-		${S3_SYNC_PROG} put ${BACKUP_DIR_NAME}/${BACKUP_SQL_FILE_NAME}  s3://${S3_BUCKET}/${BACKUP_SQL_FILE_NAME}
-	fi
-
-	if [ -e ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME} ]; then
-		rm ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME}	
-	fi
-	if [ -e ${HOME}/.s3cfg ] && [ -e ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME} ]; then
-			${S3_SYNC_PROG} rm s3://${S3_BUCKET}/${DELETE_SQL_FILE_NAME}
-	fi
-	
+  	if [ -e ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME} ]; then
+  	  	rm ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME}
+  	fi
+  	  if [ -e ${HOME}/.s3cfg ] && [ -e ${BACKUP_DIR_NAME}/${DELETE_SQL_FILE_NAME} ]; then
+  	  		${S3_SYNC_PROG} rm s3://${S3_BUCKET}/${DELETE_SQL_FILE_NAME}
+	  fi
+  done
 	return $?
 }
 
 
 case $1 in
 
-  "-b") 
+  "-b")
         if [ -f $TMP_FILE ];then
           echo "Already running,  please wait"
           return 2
@@ -92,6 +91,5 @@ case $1 in
     	echo -ne "usage\n\t-b backup\n\t-ls list backups\n\t-rm delete backup from s3 storage\n\t-get get backup to tmp\n"
     ;;
 esac
-
 
 
